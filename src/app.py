c60d9f9c20d39781399e4e2433742a05136e4096
VinodKumar KP <vinodkumar.kp.1985@gmail.com>
2025-05-02 14:45:58 -0600

    updated

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human being! <3',
+        'message': 'You are doing great, little human being1! <3',
         'deployed_on': 'kubernetes'
     })
 
